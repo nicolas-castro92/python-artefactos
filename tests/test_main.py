314c452159f0e0efccd0c2fc6f33eb0812f7e8bf
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2025-05-14 15:33:38 -0500

    Prueba de fallo en test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,8 @@ from app.main import suma, resta
 
 def test_suma():
     assert suma(3, 4) == 7
-    assert suma(-1, 1) == 0
+    # assert suma(-1, 1) == 0
+    assert suma(3, 4) == 999
 
 def test_resta():
     assert resta(4, 3) == 1
