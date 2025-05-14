80b91f6b5e3fe90466d09975e09c16da3ecb33de
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2025-05-14 16:50:38 -0500

    actualizando unittest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
-# tests/test_main.py
+import unittest
 from app.main import suma, resta
 
-def test_suma():
-    assert suma(3, 4) == 7
-    assert suma(-1, 1) == 0
+class TestCalc(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
 
-def test_resta():
-    assert resta(4, 3) == 1
-    assert resta(0, 1) == -1
+    def test_resta(self):
+        self.assertEqual(resta(5, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
