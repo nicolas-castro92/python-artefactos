bf15d06238e76827f2c5be7223c4885920f977fc
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2025-05-14 12:20:14 -0500

    First commit

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+# tests/test_main.py
+from app.main import suma, resta
+
+def test_suma():
+    assert suma(3, 4) == 7
+    assert suma(-1, 1) == 0
+
+def test_resta():
+    assert resta(4, 3) == 1
+    assert resta(0, 1) == -1
