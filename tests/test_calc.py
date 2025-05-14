350440965137d78d56e99edda2249386bfad25fd
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2025-05-14 16:35:35 -0500

    Prueba inicial copn unittest y nuevo workflow

---
+++ tests/test_calc.py
@@ -0,0 +1,13 @@
+# tests/test_calc.py
+import unittest
+from app.main import suma, resta
+
+class TestCalc(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+
+    def test_resta(self):
+        self.assertEqual(resta(5, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
